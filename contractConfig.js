c4a27d6df123387727e0c457e4fba55835becf08
aghia98 <aghimail98@gmail.com>
2022-10-29 02:53:30 +0200

    Basic Smart Contract with frontend

---
+++ contractConfig.js
@@ -0,0 +1,59 @@
+const getContractInfos= () => {
+    return {
+        BankContractAddress: "0x4968487c9542807Dd4c627AC59C18108bCD6CCEE",
+        BankContractABI: [
+            {
+                "inputs": [],
+                "name": "deposit",
+                "outputs": [],
+                "stateMutability": "payable",
+                "type": "function"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": false,
+                        "internalType": "address",
+                        "name": "_address",
+                        "type": "address"
+                    },
+                    {
+                        "indexed": false,
+                        "internalType": "uint256",
+                        "name": "amount",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "depositAdded",
+                "type": "event"
+            },
+            {
+                "inputs": [],
+                "name": "withdraw",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": false,
+                        "internalType": "address",
+                        "name": "_address",
+                        "type": "address"
+                    },
+                    {
+                        "indexed": false,
+                        "internalType": "uint256",
+                        "name": "amount",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "withdrawAdded",
+                "type": "event"
+            }
+        ]
+        }
+}
